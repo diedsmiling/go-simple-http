c3adb41e083bf4058e18c8eba4141f1d1b3e4788
lazarev <diedsmiling@gmail.com>
2018-02-12 00:11:57 +0100

    First commit

---
+++ microservice.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/api/echo", echo)
+	http.ListenAndServe(port(), nil)
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	return ":" + port
+}
+
+func index(w http.ResponseWriter, r * http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Hello Cloud Native with Go.")
+}
+
+func echo(w http.ResponseWriter, r * http.Request) {
+	message := r.URL.Query()["message"][0]
+	w.Header().Add("Content-type", "text/plain")
+	fmt.Fprint(w, message)
+}
